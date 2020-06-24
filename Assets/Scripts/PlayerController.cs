d4a3cb2f14281948194f53ad919434ebb6e7fa6d
Antti Heimonen <anttiheimonen@hotmail.com>
2020-06-24 19:43:11 +0300

    Added player's flame to ship when moving

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,19 +12,18 @@ public class PlayerController : MonoBehaviour
     private InputAction m_MoveAction;
     private InputAction m_FireAction;
     public float motorPower;
-
+    public float shipSize;  // Affects to the postion of flames
     Rigidbody2D rbody;
+    SpriteRenderer sr;
 
-    // Start is called before the first frame update
     void Start()
     {
         rbody = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
         if (m_PlayerInput == null)
         {
-            // m_PlayerInput = GetComponent<PlayerInput>();
             m_PlayerInput = (PlayerInput)gameObject.GetComponent("PlayerInput");
             m_FireAction = m_PlayerInput.actions["fire"];
-            // m_LookAction = m_PlayerInput.actions["look"];
             m_MoveAction = m_PlayerInput.actions["move"];
         }
     }
@@ -32,20 +31,18 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        var movement = m_MoveAction.ReadValue<Vector2>();
+        var movement = GetNormalizedMovement();
         if (movement != Vector2.zero)
         {
             // Get ship's RigidBody
             Rigidbody2D shipRBody = GameObject.FindGameObjectWithTag("Ship")
-            .GetComponent<Rigidbody2D>();
+                .GetComponent<Rigidbody2D>();
 
-            // Normalize movement, so it cannot cause too much force
-            movement.Normalize();
             Vector2 forceToAdd = movement * motorPower;
             Debug.Log(forceToAdd);
             shipRBody.AddForce(forceToAdd);
             // rbody.AddForce(forceToAdd);
-            transform.position = Vector2.zero;
+            // transform.position = Vector2.zero;
         }
     }
 
@@ -53,14 +50,49 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         {
-            if (m_FireAction.triggered)
-                Debug.Log("Ammutaan");
+            FlameControl();
         }
+    }
+
+
+    /// Controls the visibiliti, postition and angel if the player's flame
+    void FlameControl()
+    {
+        var movement = GetNormalizedMovement();
+        if (movement == Vector2.zero)
+        {
+            sr.enabled = false;
+            return;
+        }
+
+        sr.enabled = true;
+
+
+        // Position around ship
+        Rigidbody2D shipRBody = GameObject.FindGameObjectWithTag("Ship")
+            .GetComponent<Rigidbody2D>();
+        GameObject ship = GameObject.FindGameObjectWithTag("Ship");
+        Vector3 offset = (Vector3)GetNormalizedMovement() * shipSize;
+        transform.position = ship.transform.position - offset;
+
+        // Angle
+        var angle = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg - 180f;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
     }
 
 
     public void OnMove()
     {
+    }
+
+
+    /// Returns player's stick position as normalized Vector2 or Vector2.zero
+    Vector2 GetNormalizedMovement()
+    {
+        var movement = m_MoveAction.ReadValue<Vector2>();
+        if (movement != Vector2.zero)
+            movement.Normalize();
+        return movement;
     }
 
 
