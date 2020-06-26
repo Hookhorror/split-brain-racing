48655c48bb91bba43b91ae2b2d8b9f5f76676880
Joni Kiviluoto <joni.kiviluoto@gmail.com>
2020-06-26 16:09:38 +0300

    Made skeleton for Main Menu and added basic logic to it

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void StartScene(string scene)
+    {
+        Debug.Log("Loading " + scene + " scene");
+        SceneManager.LoadScene(scene);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting");
+        Application.Quit();
+    }
+}
