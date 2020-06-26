bd4b6f5670af431a0f878e42840dd30cdab0af47
Antti Heimonen <anttiheimonen@hotmail.com>
2020-06-26 15:57:29 +0300

    Fixed PlayerController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -19,7 +19,6 @@ public class PlayerController : MonoBehaviour
     GameObject ship;
     Rigidbody2D shipRBody;
 
-
     void Start()
     {
         playerRBody = GetComponent<Rigidbody2D>();
@@ -43,75 +42,79 @@ public class PlayerController : MonoBehaviour
     }
 
 
-    void FixedUpdate()shipRBodyt<ShipController>().ResetToLastCheckpoint();
-    checkpointResetRequested = false;
+    void FixedUpdate()
+    {
+        if (checkpointResetRequested)
+        {
+            ship.GetComponent<ShipController>().ResetToLastCheckpoint();
+            checkpointResetRequested = false;
         }
-var movement = GetNormalizedMovement();
-if (movement != Vector2.zero)
-{
-    Vector2 forceToAdd = movement * motorPower;
-    // Debug.Log(forceToAdd);
-    shipRBody.AddForce(forceToAdd);
-}
+        var movement = GetNormalizedMovement();
+        if (movement != Vector2.zero)
+        {
+            Vector2 forceToAdd = movement * motorPower;
+            // Debug.Log(forceToAdd);
+            shipRBody.AddForce(forceToAdd);
+        }
     }
 
 
     // Update is called once per frame
     void Update()
-{
     {
-        FlameControl();
-    }
-}
-
-
-/// Controls the visibiliti, postition and angel if the player's flame
-void FlameControl()
-{
-    var movement = GetNormalizedMovement();
-    if (movement == Vector2.zero)
-    {
-        sr.enabled = false;
-        return;
+        {
+            FlameControl();
+        }
     }
 
-    sr.enabled = true;
+
+    /// Controls the visibiliti, postition and angel if the player's flame
+    void FlameControl()
+    {
+        var movement = GetNormalizedMovement();
+        if (movement == Vector2.zero)
+        {
+            sr.enabled = false;
+            return;
+        }
+
+        sr.enabled = true;
 
 
-    // Position around ship
-    Vector3 offset = (Vector3)GetNormalizedMovement() * shipSize;
-    transform.position = ship.transform.position - offset;
+        // Position around ship
+        Vector3 offset = (Vector3)GetNormalizedMovement() * shipSize;
+        transform.position = ship.transform.position - offset;
 
-    // Angle
-    var angle = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg - 180f;
-    transform.rotation = Quaternion.Euler(0, 0, angle);
-}
-
-
-public void OnMove()
-{
-}
+        // Angle
+        var angle = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg - 180f;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
 
 
-/// Returns player's stick position as normalized Vector2 or Vector2.zero
-Vector2 GetNormalizedMovement()
-{
-    var movement = m_MoveAction.ReadValue<Vector2>();
-    if (movement != Vector2.zero)
-        movement.Normalize();
-    return movement;
-}
+    public void OnMove()
+    {
+    }
 
 
-public void OnFire()
-{
-    // Debug.Log("Checkpoint palautus");
-    checkpointResetRequested = true;
-}
+    /// Returns player's stick position as normalized Vector2 or Vector2.zero
+    Vector2 GetNormalizedMovement()
+    {
+        var movement = m_MoveAction.ReadValue<Vector2>();
+        if (movement != Vector2.zero)
+            movement.Normalize();
+        return movement;
+    }
 
 
-public void Shoot()
-{
-    // Debug.Log("Ampuu");
-}
+    public void OnFire()
+    {
+        // Debug.Log("Checkpoint palautus");
+        checkpointResetRequested = true;
+    }
+
+
+    public void Shoot()
+    {
+        // Debug.Log("Ampuu");
+    }
 }
