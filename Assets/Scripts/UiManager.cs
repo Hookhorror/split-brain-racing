bfb24308e11d963a69154778ad1728b649c91120
Joni Kiviluoto <joni.kiviluoto@gmail.com>
2020-06-28 17:53:47 +0300

    Added spritesheet for the ship Added UiManager

---
+++ Assets/Scripts/UiManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class UiManager : MonoBehaviour
+{
+    public static UiManager uiManager;
+    private void Awake()
+    {
+        uiManager = this;
+    }
+
+    public TextMeshProUGUI currentTime;
+    public TextMeshProUGUI splitTime;
+}
