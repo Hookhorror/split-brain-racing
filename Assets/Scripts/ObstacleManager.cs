182de0768583d50db6df02ae123fce7b6bf34807
Joni Kiviluoto <joni.kiviluoto@gmail.com>
2020-07-05 15:55:44 +0300

    Added graphics for whirl and made it spin

---
+++ Assets/Scripts/ObstacleManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleManager : MonoBehaviour
+{
+    public static ObstacleManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public float whirlRotationTime;
+}
---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    private float rotationSpeed;
+    private float size;
+    private float angleToRotate;
+
+    private void Start()
+    {
+        rotationSpeed = ObstacleManager.instance.whirlRotationTime;
+        size = transform.localScale.x;
+        angleToRotate = rotationSpeed / (size * size);
+        Debug.Log(angleToRotate);
+    }
+    void Update()
+    {
+        // float angle = Mathf.Lerp(0, angleToRotate, 0.5f);
+        // Debug.Log(angle);
+        // transform.Rotate(transform.rotation.x, transform.rotation.y, transform.rotation.z + angleToRotate);
+        transform.RotateAround(Vector3.forward, angleToRotate);
+        // transform.Rotate();
+
+    }
+}
